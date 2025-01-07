7fa101e1fd609006e8051d2677185b2215ddf780
Dan Pock <self@danpock.me>
2025-01-07 13:44:14 -0500

    make hack/deps.mk use generic indexes for Kubectl vars

--- hack/make/deps.mk
+++ hack/make/deps.mk
@@ -1,26 +1,25 @@
-KUBECTL28_VERSION := v1.28.14
-KUBECTL28_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL28_VERSION)/bin/linux/arm64/kubectl.sha256")
-KUBECTL28_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL28_VERSION)/bin/linux/amd64/kubectl.sha256")
+KUBECTL1_VERSION := v1.29.12
+KUBECTL1_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL1_VERSION)/bin/linux/arm64/kubectl.sha256")
+KUBECTL1_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL1_VERSION)/bin/linux/amd64/kubectl.sha256")
 
-KUBECTL29_VERSION := v1.29.9
-KUBECTL29_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL29_VERSION)/bin/linux/arm64/kubectl.sha256")
-KUBECTL29_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL29_VERSION)/bin/linux/amd64/kubectl.sha256")
+KUBECTL2_VERSION := v1.30.8
+KUBECTL2_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL2_VERSION)/bin/linux/arm64/kubectl.sha256")
+KUBECTL2_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL2_VERSION)/bin/linux/amd64/kubectl.sha256")
 
-KUBECTL30_VERSION := v1.30.5
-KUBECTL30_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL30_VERSION)/bin/linux/arm64/kubectl.sha256")
-KUBECTL30_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL30_VERSION)/bin/linux/amd64/kubectl.sha256")
+KUBECTL3_VERSION := v1.31.4
+KUBECTL3_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL3_VERSION)/bin/linux/arm64/kubectl.sha256")
+KUBECTL3_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL3_VERSION)/bin/linux/amd64/kubectl.sha256")
 
-KUBECTL31_VERSION := v1.31.1
-KUBECTL31_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL31_VERSION)/bin/linux/arm64/kubectl.sha256")
-KUBECTL31_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL31_VERSION)/bin/linux/amd64/kubectl.sha256")
+KUBECTL4_VERSION := v1.32.0
+KUBECTL4_SUM_arm64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL4_VERSION)/bin/linux/arm64/kubectl.sha256")
+KUBECTL4_SUM_amd64 ?= $(shell curl -L "https://dl.k8s.io/release/$(KUBECTL4_VERSION)/bin/linux/amd64/kubectl.sha256")
 
+KUBECTL1 := "$(KUBECTL1_VERSION):$(KUBECTL1_SUM_arm64):$(KUBECTL1_SUM_amd64)"
+KUBECTL2 := "$(KUBECTL2_VERSION):$(KUBECTL2_SUM_arm64):$(KUBECTL2_SUM_amd64)"
+KUBECTL3 := "$(KUBECTL3_VERSION):$(KUBECTL3_SUM_arm64):$(KUBECTL3_SUM_amd64)"
+KUBECTL4 := "$(KUBECTL4_VERSION):$(KUBECTL4_SUM_arm64):$(KUBECTL4_SUM_amd64)"
 
-KUBECTL28 := "$(KUBECTL28_VERSION):$(KUBECTL28_SUM_arm64):$(KUBECTL28_SUM_amd64)"
-KUBECTL29 := "$(KUBECTL29_VERSION):$(KUBECTL29_SUM_arm64):$(KUBECTL29_SUM_amd64)"
-KUBECTL30 := "$(KUBECTL30_VERSION):$(KUBECTL30_SUM_arm64):$(KUBECTL30_SUM_amd64)"
-KUBECTL31 := "$(KUBECTL31_VERSION):$(KUBECTL31_SUM_arm64):$(KUBECTL31_SUM_amd64)"
-
-KUBECTL_VERSION_INFO := "$(KUBECTL28) $(KUBECTL29) $(KUBECTL30) $(KUBECTL31)"
+KUBECTL_VERSION_INFO := "$(KUBECTL1) $(KUBECTL2) $(KUBECTL3) $(KUBECTL4)"
 
 # Reduces the code duplication on Makefile by keeping all args into a single variable.
 IMAGE_ARGS := --build-arg KUBECTL_VERSION_INFO=$(KUBECTL_VERSION_INFO)
